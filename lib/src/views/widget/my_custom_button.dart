072a2bce1210425d2d53fff754d658c6af05303b
USER <shakib501886@gmail.com>
2025-04-19 00:49:02 +0600

    app bar added

---
+++ lib/src/views/widget/my_custom_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyCustomButton extends StatelessWidget {
+  const MyCustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
